3d16227422add9560528dff747e1501355475430
NicolasWillyam <phambahuyhoang0@gmail.com>
2024-09-07 09:18:34 +0700

    Add Authentication Clerk

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,13 @@
 import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/header";
 import React from "react";
 
 const Page = () => {
   return (
     <div>
+      <Header>
+        <p>Header</p>
+      </Header>
       <Editor />
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
+import { dark } from "@clerk/themes";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,12 +27,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorPrimary: "#3371FF",
+          fontSize: "16px",
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn("min-h-screen antialiased")}>
+          {/* <Provider>{children}</Provider> */}
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
