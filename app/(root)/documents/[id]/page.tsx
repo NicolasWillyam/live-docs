21121b1e2a06f3695e3c08203d87de539ee0f7e6
NicolasWillyam <phambahuyhoang0@gmail.com>
2024-09-07 08:39:53 +0700

    First init

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { Editor } from "@/components/editor/Editor";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
+};
+
+export default Page;
